12eadab581bcbd54df311a3c41519ceef899b3a3
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2024-02-05 11:04:44 +0200

    rename file

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const searchRouter = require("./services/search");
+
+const app = express();
+
+app.use(cors()); 
+
+app.use("/search", searchRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
