246e75b169e93e56fc3ea3a04b480498cc7e93cf
AsmaaNabilBakr <Asmaa.nabilbakr@gmail.com>
2024-02-05 12:03:44 +0200

    update server

--- server/index.js
+++ server/index.js
@@ -5,16 +5,16 @@ const searchRouter = require("./services/search");
 const app = express();
 
 app.use(cors());
-// app.use("/", (req, res) => {
-//   res.send("server is running.");
-// });
+
 app.use("/search", searchRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
-
+app.use("/", (req, res) => {
+  res.send("server is running.");
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
